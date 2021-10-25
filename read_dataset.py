7dc87db876ffa53452f9037aee20464db3f8b233
ae3718 <ae3718@ic.ac.uk>
2021-10-25 10:48:15 +0100

    Created function to read data

---
+++ read_dataset.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def read_dataset(filepath):
+    """ Read in the dataset from the specified filepath
+
+    Args:
+        filepath (str): The filepath to the dataset file
+
+    Returns:
+        tuple: returns a tuple of (x, y, classes), each being a numpy array. 
+               - x is a numpy array with shape (N, K), 
+                   where N is the number of instances
+                   K is the number of features/attributes
+               - y is a numpy array with shape (N, ), and each element should be 
+                   an integer from 0 to C-1 where C is the number of classes 
+               - classes : a numpy array with shape (C, ), which contains the 
+                   unique class labels corresponding to the integers in y
+    """
+    x = []
+    y = []
+    for line in open(filepath):
+        if line.strip() != "": # handle empty rows in file
+            row = line.rstrip().split('\t')
+            x.append(list(map(float, row[:-1]))) 
+            y.append(row[-1])
+
+    x = np.array(x)
+    y = np.array(y)
+    return (x, y)
+
+#x, y = read_dataset("wifi_db/clean_dataset.txt")
