2ba3edfbff0e68d91f65c2fce2629ee16b406722
Jaime Sabal <jsabalb@gmail.com>
2021-10-25 19:15:44 +0100

    Changed main.py so it runs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from classifier.tree import Node, DecisionTreeClassifier, compute_accuracy
-from read_dataset import read_dataset
-from split_dataset import split_dataset
+from classifier.tree import DecisionTreeClassifier, compute_accuracy
+from data_manipulation.read_dataset import read_dataset
+from data_manipulation.split_dataset import split_dataset
 
 
 if __name__ == '__main__':
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = split_dataset(X, y, test_proportion = 0.2)  ## change the split
     tree_clf = DecisionTreeClassifier(max_depth=100)
     tree_clf.fit(X_train, y_train)
-    y_predicted = tree_clf.predict(X_test)
-    print(f"The training accuracy is: {compute_accuracy(y_train, y_predicted)}")
-    print(f"The test accuracy is: {compute_accuracy(y_test, y_predicted)}")
+    y_predicted_train = tree_clf.predict(X_train)
+    y_predicted_test = tree_clf.predict(X_test)
+
+    print(f"The training accuracy is: {compute_accuracy(y_train, y_predicted_train)}")
+    print(f"The test accuracy is: {compute_accuracy(y_test, y_predicted_test)}")
