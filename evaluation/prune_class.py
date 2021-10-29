52f677c6203e8049151cd09403f8d43143b18b8b
Jaime Sabal <jsabalb@gmail.com>
2021-10-29 18:54:50 +0100

    Added 2nd version of pruning class

--- evaluation/prune_class.py
+++ evaluation/prune_class.py
@@ -1,7 +1,8 @@
 from evaluation.evaluation_metrics import EvaluationMetrics
+from classifier.node import Node
 import numpy as np
 
-class TreePruning:
+class TreePruning1:
     def __init__(self, trained_tree, x_train, x_test, y_train, y_test):
         self.trained_tree = trained_tree
         self.try_prune_tree = trained_tree
@@ -114,3 +115,123 @@ class TreePruning:
         self.sort_depth()
         self.prune()
         return self.trained_tree
+
+
+
+
+class TreePruning2:
+    def __init__(self, trained_tree, x_train, x_test, y_train, y_test):
+        self.trained_tree = trained_tree
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.checked_nodes = []
+
+    
+    def query_update_tree(self,node_to_find, node):
+
+        if node is None:
+            return    
+        else:
+            pass
+
+        if node.left_daughter is not None:
+            self.query_update_tree(node_to_find, node.left_daughter)
+
+        if (node.depth == node_to_find.depth) and (node.feature_num == node_to_find.feature_num) and (node.split_val == node_to_find.split_val):
+            node.left_daughter = None
+            node.right_daughter = None
+            node.is_leaf = True
+            return node
+
+        if node.right_daughter is not None:
+            self.query_update_tree(node_to_find, node.right_daughter)
+
+    def prune_left_most_parent(self, node):
+        ''' Find next deepest, left most node for pruning. Track trained tree in parallel to 
+            potentially update its node
+        '''
+        if node is None:
+            return
+
+        if node.left_daughter.is_leaf == False and node.left_daughter not in self.checked_nodes:
+            self.prune_left_most_parent(node.left_daughter)
+        
+        if node.left_daughter.is_leaf == True and node.left_daughter not in self.checked_nodes:
+            if node.right_daughter.is_leaf == True and node not in self.checked_nodes:
+                # Add left and right data
+                node_to_find = Node()
+                node_to_find.depth = node.depth 
+                node_to_find.feature_num = node.feature_num
+                node_to_find.split_val = node.split_val
+                node.left_daughter = None
+                node.right_daughter = None
+                node.is_leaf = True
+                self.checked_nodes.append(node)
+            else:
+                self.prune_left_most_parent(node.right_daughter)
+        else:
+            return None
+
+        return node
+
+    def compute_accuracy(self, y, y_predicted):
+        """
+        """
+        assert len(y) == len(y_predicted)
+        try:
+            return np.sum(y == y_predicted) / len(y)
+        except ZeroDivisionError:
+            return 0
+         
+
+    def update_trained_tree(self, tree1, tree2):
+        """
+        """
+        #metrics = EvaluationMetrics() 
+
+        y_predicted_test_pre_pruning = np.array(self.predict_tree(self.x_test, tree = tree1))
+        y_predicted_test_post_pruning = np.array(self.predict_tree(self.x_test, tree = tree2))
+        tree_accuracy_post_pruning= self.compute_accuracy(self.y_test, y_predicted_test_post_pruning)
+        tree_accuracy_pre_pruning = self.compute_accuracy(self.y_test,y_predicted_test_pre_pruning) 
+
+        return  tree_accuracy_post_pruning >= tree_accuracy_pre_pruning
+
+    def predict_tree(self, X, tree):
+        """
+        """
+        predicted_values = []
+        for sample in X:
+            node = tree
+            while not node.is_leaf:
+                if sample[node.feature_num] < node.split_val:
+                    node = node.left_daughter
+                else:
+                    node = node.right_daughter
+            predicted_values.append(node.predicted_room)
+
+        return predicted_values
+        
+
+    def prune_tree(self):
+        """"""
+        #pre_pruned_tree = self.trained_tree
+        #node = pre_pruned_tree
+        #node_is_parent = (node.left_daughter.is_leaf == True) and (node.right_daughter.is_leaf == True)
+        pre_pruned_tree = self.trained_tree
+        if self.prune_left_most_parent(pre_pruned_tree) == None:
+            print(pre_pruned_tree)
+            y_predicted_test_pre_pruning = np.array(self.predict_tree(self.x_test, tree = pre_pruned_tree))
+            print(self.compute_accuracy(self.y_test, y_predicted_test_pre_pruning))
+            return pre_pruned_tree
+        else:
+            post_pruned_tree = self.prune_left_most_parent(pre_pruned_tree)
+
+            #update left and right daughters to none and isleaf.yes
+            #do not know how to concatenate node and path (node+path).isleaf = True
+            if self.update_trained_tree(tree1 = pre_pruned_tree, tree2 = post_pruned_tree):
+                self.query_update_tree(node_to_find=post_pruned_tree, node=pre_pruned_tree)  
+
+            self.trained_tree = post_pruned_tree
+            self.prune_tree()
